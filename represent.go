163795765376290599abe33f5d0a7cad35c207a0
EchoSun <echosun1996@126.com>
2021-01-26 20:01:02 +0800

    不再显示行号

--- represent.go
+++ represent.go
@@ -2,30 +2,23 @@ package datautil
 
 import "fmt"
 
-// DataRepresentation 用于显示二维数组前‘top’条数据的方法
+// DataRepresentation 用于显示数据的前部分行。
 // 要求传入的数据第一行是标题。
 // 传入第一个参数是数据，第二个参数是显示前多少行。
 func DataRepresentation(data [][]string, top int) {
-	if top < 0 {
-		panic("'top' should >0!")
-	}
-	if top >= len(data) {
-		panic("'top' should smaller than data's size!")
-	}
 	for ix, value := range data {
 		for itemIndex, item := range value {
 			if itemIndex == 0 {
-				if ix == 0 {
-					fmt.Print("Head:\n|   ")
-				} else if ix == 1 {
-					fmt.Println("Body:")
-				}
-				if itemIndex == 0 && ix != 0 {
-					fmt.Print("| ", ix)
-				}
+				//if ix == 0 {
+				//	fmt.Println("Head:")
+				//} else if ix == 1 {
+				//	fmt.Println("Body:")
+				//}
+				// 第一列开头没有空格
+				fmt.Print("| " + item)
+			} else {
+				fmt.Print(" | " + item)
 			}
-			fmt.Print(" | " + item)
-
 		}
 		fmt.Println(" |")
 		if ix == top {
